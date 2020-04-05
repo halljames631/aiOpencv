6b4b406f3a0c3223d1c9a66706c0a3da67b051d6
james hall <halljames631@gmail.com>
2020-04-04 20:59:50 -0400

    added openCV6-mouseClick.py file

---
+++ openCV/openCV6-mouseClick.py
@@ -0,0 +1,32 @@
+import cv2
+
+print(cv2.__version__)
+evt = -1
+def click(event,x,y,flags,params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        global pnt 
+        global evt
+        print('mouse event was: ', event)
+        print(x,',',y)
+        pnt = (x,y)
+        evt = event
+
+cv2.namedWindow('picam')
+cv2.setMouseCallback('picam',click)
+
+cam = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cam.read()
+
+    if evt == 1:
+        cv2.circle(frame,pnt,5,(0,0,255),-1)
+
+    cv2.imshow('picam', frame)
+    cv2.moveWindow('picam',0,0)
+
+    if cv2.waitKey(1) == ord ('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
