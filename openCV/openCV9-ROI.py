7be951f450955a184d32db10efc7740941c99764
james hall <halljames631@gmail.com>
2020-04-05 10:36:31 -0400

    added ROI

--- openCV/openCV9-ROI.py
+++ openCV/openCV9-ROI.py
@@ -7,8 +7,21 @@ cam = cv2.VideoCapture(0)
 while True:
     ret, frame = cam.read()
 
+    roi = frame[50:250,200:400].copy()
+
+    roiGray = cv2.cvtColor(roi,cv2.COLOR_BGR2GRAY)
+    roiGray = cv2.cvtColor(roiGray,cv2.COLOR_GRAY2BGR)
+
+    frame[50:250,200:400] = roiGray
+
+    cv2.imshow('ROI',roi)
     cv2.imshow('picam', frame)
+    cv2.imshow('gray',roiGray)
+
     cv2.moveWindow('picam',0,0)
+    cv2.moveWindow('ROI',705,0)
+    cv2.moveWindow('gray',705,250)
+
     if cv2.waitKey(1) == ord ('q'):
         break
 
