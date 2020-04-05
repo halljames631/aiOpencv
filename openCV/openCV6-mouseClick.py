7234aa8e3b4c5df3a5e3c36a9b1844faff81c8a0
james hall <halljames631@gmail.com>
2020-04-04 21:31:39 -0400

    adding points

--- openCV/openCV6-mouseClick.py
+++ openCV/openCV6-mouseClick.py
@@ -2,6 +2,7 @@ import cv2
 
 print(cv2.__version__)
 evt = -1
+coord = []
 def click(event,x,y,flags,params):
     if event == cv2.EVENT_LBUTTONDOWN:
         global pnt 
@@ -9,6 +10,8 @@ def click(event,x,y,flags,params):
         print('mouse event was: ', event)
         print(x,',',y)
         pnt = (x,y)
+        coord.append(pnt)
+        print(coord)
         evt = event
 
 cv2.namedWindow('picam')
@@ -19,17 +22,22 @@ cam = cv2.VideoCapture(0)
 while True:
     ret, frame = cam.read()
 
-    if evt == 1:
-        cv2.circle(frame,pnt,5,(0,0,255),-1)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        myStr = str(pnt)
-        cv2.putText(frame,myStr,pnt,font,1,(255,0,0),2)
+    for pnts in coord:
+        cv2.circle(frame,pnts,5,(0,0,255),-1)
+        # font = cv2.FONT_HERSHEY_SIMPLEX
+        # myStr = str(pnt[pnts])
+        # cv2.putText(frame,myStr,pnts[pnts],font,1,(255,0,0),2)
 
     cv2.imshow('picam', frame)
     cv2.moveWindow('picam',0,0)
 
-    if cv2.waitKey(1) == ord ('q'):
+    keyEvent = cv2.waitKey(1)
+
+    if keyEvent == ord ('q'):
         break
+
+    if keyEvent == ord('c'):
+        coord.clear()
 
 cam.release()
 cv2.destroyAllWindows()
