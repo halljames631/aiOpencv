71c683e47b8e8e7f528154804fda3c95ca93e85a
james hall <halljames631@gmail.com>
2020-04-05 09:44:45 -0400

    added color picker

--- openCV/openCV6-mouseClick.py
+++ openCV/openCV6-mouseClick.py
@@ -1,8 +1,11 @@
 import cv2
+import numpy as np
 
 print(cv2.__version__)
 evt = -1
 coord = []
+img = np.zeros((250,250,3),np.uint8)
+
 def click(event,x,y,flags,params):
     if event == cv2.EVENT_LBUTTONDOWN:
         global pnt 
@@ -18,7 +21,16 @@ def click(event,x,y,flags,params):
         blue = frame[y,x,0]
         green = frame[y,x,1]
         red = frame[y,x,2]
-        print(blue)
+        print(blue,green,red)
+        colorString = str(blue) + ',' + str(green) + ',' + str(red)
+        img[:] = [blue,green,red]
+        fnt = cv2.FONT_HERSHEY_PLAIN
+        r = 255 - int(red)
+        g = 255 - int(green)
+        b = 255 - int(blue)
+        tp = (b,g,r)
+        cv2.putText(img,colorString,(10,25),fnt,1,tp,2)
+        cv2.imshow('mycolor',img)
 
 cv2.namedWindow('picam')
 cv2.setMouseCallback('picam',click)
